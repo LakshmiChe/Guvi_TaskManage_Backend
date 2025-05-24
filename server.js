6709bc8a632f2a1ac74551d80ebbb37b1f8ad6b0
LakshmiChe <14uece050@gmail.com>
2025-05-24 06:24:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-// const cron = require("node-cron");
-const multer = require('multer');
-const fs = require('fs');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 const taskRoutes = require('./routes/taskRoutes');
@@ -26,20 +23,10 @@ connectDB();
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-// Cron Job for Daily Deadline Reminders
-// cron.schedule("0 8 * * *", async () => {
-//     console.log("Running daily deadline reminder cron job...");
-//     await sendDeadlineReminders();
-// });
 // Error handling middleware
 app.use((err, req, res, next) => {
     res.status(500).json({ message: err.message });
 });
 
-// Start the server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-// Export as serverless function
+// Export for serverless
 module.exports = serverless(app);
